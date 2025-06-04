b8604caedefe6dd428124186be12193b28fa5c29
tomaszz-dev <tomaszzawacki6@gmail.com>
2025-06-04 17:23:35 +0200

    update
    -card sound added
    -xyz adjustments

--- scenes/game/level.gd
+++ scenes/game/level.gd
@@ -2,20 +2,42 @@ extends Node3D
 
 # Ładujemy prefab (PackedScene) karty
 var CardScene := preload("res://scenes/objects/card.tscn")
-var gracz
+var sound = preload("res://assets/sounds/card_rozdanie.wav")
+var player = AudioStreamPlayer.new()
 var lista = []
 var gracz1 = []
 var gracz2 = []
+var przesuniecie_x
+var przesuniecie_y
+var przesuniecie_z
 
+
+	
 func _ready():
 	losowanie()
-	var przesuniecie_x = -60
-	var przesuniecie_y = 83
+	dzwiek()
+	#gracz1
+	przesuniecie_x = -30
+	przesuniecie_z = -60
+	przesuniecie_y = 82.3
 	for i in gracz1:
-		karta(przesuniecie_x,i,przesuniecie_y)
-		przesuniecie_x = przesuniecie_x+5
-		przesuniecie_y = przesuniecie_y+0.1
-		await get_tree().create_timer(0.1).timeout
+		karta(przesuniecie_x,przesuniecie_y,przesuniecie_z,i,0)
+		przesuniecie_z = przesuniecie_z+5
+		przesuniecie_y = przesuniecie_y+0.01
+		await get_tree().create_timer(0.01).timeout
+
+	#gracz2
+	przesuniecie_x = 40
+	przesuniecie_z = -60
+	przesuniecie_y = 83
+	for i in gracz2:
+		karta(przesuniecie_x,przesuniecie_y,przesuniecie_z,i,180)
+		przesuniecie_z = przesuniecie_z+5
+		przesuniecie_y = przesuniecie_y+0.01
+		await get_tree().create_timer(0.01).timeout
+
+
+
 
 func losowanie():
 	for i in range(0,52):
@@ -29,14 +51,20 @@ func losowanie():
 			gracz2.append(i)
 	print(gracz1,gracz2)
 
-func karta(z,card_id,y):
+
+func karta(x,y,z,card_id,rotacja):
 		# Tworzymy instancję karty
 	var card = CardScene.instantiate()
 	
 	# Ustawiamy jej pozycję w świecie (opcjonalnie)
-	card.transform.origin = Vector3(-34.76, y, z)
-	card.rotation = Vector3(0, deg_to_rad(90), 0)
+	card.transform.origin = Vector3(x, y, z)
+	card.rotation = Vector3(0, deg_to_rad(90), deg_to_rad(rotacja))
 	# Dodajemy do sceny
 	add_child(card)
 	# Ustawiamy jej wartość (np. 9 kier)
 	card.set_card(card_id)
+
+func dzwiek():
+	add_child(player)
+	player.stream = sound
+	player.play()
